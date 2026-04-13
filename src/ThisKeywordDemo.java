febc54c966262338cfc49b7e3814de58fd07e9c5
Vish_online <vishnuonline2218@gmail.com>
2026-04-13 13:13:20 +0530

    Update ThisKeywordDemo.java

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 class ThisDemo {
     int value;
 
-    // TODO: Define setValue(int value)
-    // Use 'this.value = value' to resolve naming conflict
+    void setValue(int value) {
+        this.value = value;
+    }
     
-    // TODO: Define display() to print "Value: " + value
+    void display() {
+        System.out.println("Value: " + value);
+    }
 }
 
 public class ThisKeywordDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read the integer input
+        int inputVal = sc.nextInt();
         
-        // TODO: Create ThisDemo object
-        // TODO: Call setValue() with the input
-        // TODO: Call display()
+        ThisDemo demo = new ThisDemo();
+        demo.setValue(inputVal);
+        demo.display();
+        sc.close();
     }
 }
